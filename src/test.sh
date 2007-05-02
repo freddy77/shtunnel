8c2bc782de90ac3bb04ca1fe61e6aa1fee2255b4
Frediano Ziglio <freddy77@gmail.com>
2007-05-02 09:25:25 +0000

    add automated test

---
+++ src/test.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# $Id: test.sh,v 1.1 2007-05-02 09:25:25 freddy Exp $
+
+# shtunnel --debug --shell ls xxx < /dev/null
+#  should work and not stall
+OUT=`./shtunnel --debug --shell ls shtunnel < /dev/null 2> /dev/null | if read -t 2 XXX; then
+	echo -n $XXX | cat -ve
+fi`
+EXP='shtunnel^M'
+if test "$OUT" != "$EXP"; then
+	echo "Wrong result '$OUT' expected '$EXP'" >&2
+	exit 1
+fi
+
+# test reset color inside line
+OUT=`./classifier --color sh -c 'echo -n ciao >&2; echo pippo' | cat -ve`
+EXP='^[[00;31mciao^[[00mpippo$'
+if test "$OUT" != "$EXP"; then
+	echo "Wrong result '$OUT' expected '$EXP'" >&2
+	exit 1
+fi
+
+# test reser color at end of line
+OUT=`./classifier --color sh -c 'echo -n ciao >&2' | cat -ve`
+rm log.txt
+EXP='^[[00;31mciao^[[00m'
+if test "$OUT" != "$EXP"; then
+	echo "Wrong result '$OUT' expected '$EXP'" >&2
+	exit 1
+fi
+
+echo All tests passed!
+exit 0
+
