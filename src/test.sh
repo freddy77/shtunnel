1f504fe339d033b2e505c5e67b8e4596bdcf3b4f
Frediano Ziglio <freddy77@gmail.com>
2007-05-30 13:09:30 +0000

    new test for buffer

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# $Id: test.sh,v 1.4 2007-05-27 07:54:50 freddy Exp $
+# $Id: test.sh,v 1.5 2007-05-30 13:09:30 freddy Exp $
 
 # shtunnel --debug --shell ls xxx < /dev/null
 #  should work and not stall
@@ -25,6 +25,14 @@ fi
 OUT=`./classifier --color sh -c 'echo -n ciao >&2' | cat -ve`
 rm log.txt
 EXP='^[[00;31mciao^[[00m'
+if test "$OUT" != "$EXP"; then
+	echo "$LINENO: Wrong result '$OUT' expected '$EXP'" >&2
+	exit 1
+fi
+
+# test no buffer overflow
+OUT=`./shtunnel --shell "sh -c" "cat ./shtunnel.c" | md5sum`
+EXP=`md5sum < ./shtunnel.c`
 if test "$OUT" != "$EXP"; then
 	echo "$LINENO: Wrong result '$OUT' expected '$EXP'" >&2
 	exit 1
